f6ecc9764c44db43a83ff41f06f0057b9820b1f5
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-10-20 15:10:53 +0300

    feat: add brackets functionallity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let newArr = bracketsConfig.map(item => item.join(''));
+
+	for (let i = 0; i < newArr.length; i++) {
+
+		if (str.includes(`${newArr[i]}`)) {
+			str = str.replace(`${newArr[i]}`, '');
+			i = -1;
+		}
+	}
+
+	if (str) {
+		return false
+	} else {
+		return true;
+	}
 }
